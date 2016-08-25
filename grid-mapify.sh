3a49c008210a1ca99dedc5211b9f80736be4d71e
Ian Johnson <ijjorama@gmail.com>
2016-08-25 16:56:33 +0100

    Add comments on input and output format

--- grid-mapify.sh
+++ grid-mapify.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+
+#
+# Output from openssl be like:
+# subject= /C=UK/O=eScience/OU=CLRC/L=RAL/CN=ceph-dev-gw3.gridpp.rl.ac.uk
+#
+# Mapfile entry we want be like:
+# "/C=UK/O=eScience/OU=CLRC/L=RAL/CN=ceph-dev-gw3.gridpp.rl.ac.uk" testuser
+#
+
 cert=$1
 user=$2
 
